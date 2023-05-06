0df9f6418b2ba9aaa5a492d7dd0e420cd2b6450f
Pedro Alencar <pedromariutti@gmail.com>
2023-05-06 12:51:23 -0300

    Resolução do exercício 10 da lista

--- src/semana01/Exercicio.java
+++ src/semana01/Exercicio.java
@@ -8,6 +8,8 @@ public class Exercicio {
     public static void main(String[] args) {
         ArrayList<String> alunos = new ArrayList<>();
         ArrayList<double[]> boletim = new ArrayList<>();
+        ArrayList<ArrayList<String>> aprovados = new ArrayList<>();
+        ArrayList<ArrayList<String>> rerovados = new ArrayList<>();
         boolean continuar = true;
 
         while (continuar){
@@ -29,6 +31,7 @@ public class Exercicio {
         }
         for (String aluno: alunos) {
             int index = alunos.indexOf(aluno);
+            ArrayList<String> resultadoAluno = new ArrayList<>();
             double somaNotas = 0;
             for (double nota: boletim.get(index)
                  ) {
@@ -37,14 +40,30 @@ public class Exercicio {
             double media = somaNotas/(boletim.get(index).length);
             System.out.printf("Aluno(a) %s.\n", aluno);
             System.out.printf("Notas: %s.\n", Arrays.toString(boletim.get(index)));
-            System.out.printf("A média do(a) aluno(a) %s, com as notas %s, foi de %.2f", aluno,
+            System.out.printf("A média do(a) aluno(a) %s, com as notas %s, foi de %.2f \n", aluno,
                     Arrays.toString(boletim.get(index)), media);
+            resultadoAluno.add(aluno);
+            resultadoAluno.add(Arrays.toString(boletim.get(index)));
+            resultadoAluno.add(String.format("%.2f", media));
 
+            if(media >= 7){
+                aprovados.add(resultadoAluno);
+            }else{
+                rerovados.add(resultadoAluno);
+            }
+
+        }
+        System.out.println("-----------------------------");
+        System.out.println("Lista de alunos aprovados:");
+        for ( ArrayList<String> aprovado : aprovados){
+            System.out.printf("Aluno(a) %s com notas %s e media %s \n",aprovado.get(0).toString(),
+                    aprovado.get(1).toString(), aprovado.get(2).toString());
+        }System.out.println("Lista de alunos reprovados:");
+        for ( ArrayList<String> reprovado : rerovados){
+            System.out.printf("Aluno(a) %s com notas %s e media %s \n",reprovado.get(0).toString(),
+                    reprovado.get(1).toString(), reprovado.get(2).toString());
         }
     }
 }
 
-//    TODO 9 - Faça uma alteração para calcular a média e exibir junto com a lista de alunos e suas notas
-
-//    TODO 10 - Por fim faça uma nova alteração mostrando a lista dos aprovados e reprovados, mostrando nome e média,
-//    para tanto a média deve ser maior ou igual a 7.0
+//   TODO 10 - Por fim faça uma nova alteração mostrando a lista dos aprovados e reprovados, mostrando nome e média, para tanto a média deve ser maior ou igual a 7.0
