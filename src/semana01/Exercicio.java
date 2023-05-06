d3ab556684d82bd539eb11d2f27476c710b96c8c
Pedro Alencar <pedromariutti@gmail.com>
2023-05-06 11:26:15 -0300

    Resolução do exercício 09 da lista

--- src/semana01/Exercicio.java
+++ src/semana01/Exercicio.java
@@ -29,15 +29,22 @@ public class Exercicio {
         }
         for (String aluno: alunos) {
             int index = alunos.indexOf(aluno);
+            double somaNotas = 0;
+            for (double nota: boletim.get(index)
+                 ) {
+                somaNotas += nota;
+            }
+            double media = somaNotas/(boletim.get(index).length);
             System.out.printf("Aluno(a) %s.\n", aluno);
             System.out.printf("Notas: %s.\n", Arrays.toString(boletim.get(index)));
+            System.out.printf("A média do(a) aluno(a) %s, com as notas %s, foi de %.2f", aluno,
+                    Arrays.toString(boletim.get(index)), media);
 
         }
     }
 }
 
-//    TODO 8 - Quando então digitado FIM, deverá exibir então a lista de alunos e suas notas
-
 //    TODO 9 - Faça uma alteração para calcular a média e exibir junto com a lista de alunos e suas notas
 
-//    TODO 10 - Por fim faça uma nova alteração mostrando a lista dos aprovados e reprovados, mostrando nome e média, para tanto a média deve ser maior ou igual a 7.0
+//    TODO 10 - Por fim faça uma nova alteração mostrando a lista dos aprovados e reprovados, mostrando nome e média,
+//    para tanto a média deve ser maior ou igual a 7.0
